c4d1d5cea695097a99d1b8aa451dd4fabbb0c7c4
kovalov-helfie <serhii.k@peiko.space>
2024-09-30 17:28:53 +0300

    fix(web): web3-btn

--- apps/web/src/components/user-component.tsx
+++ apps/web/src/components/user-component.tsx
@@ -1,9 +1,10 @@
 import { Stack, Button, Checkbox } from "@chakra-ui/react";
+import { Web3Button } from "./web3-button";
 
 export function UserComponent({userData}: {userData: any}) {
     return (
         <Stack direction={"row"} justifyContent={'space-between'} margin={'30px'}>
-            <w3m-button />
+            <Web3Button/>
             <Stack direction={'row'}>
                 {
                     <Button disabled>
---
+++ apps/web/src/components/web3-button.tsx
@@ -0,0 +1,29 @@
+import { Button } from "@chakra-ui/react"
+import { useAppKit } from "@reown/appkit/react"
+import { useAccount } from "wagmi"
+import { useCreateUser } from "../hooks/users/use-create-user"
+import { useEffect } from "react"
+import { useGetUser } from "../hooks/users/use-get-user"
+
+export const Web3Button = () => {
+    const { address } = useAccount()
+    const { open, close } = useAppKit()
+    const useCreateUserMutation = useCreateUser()
+    const { isLoadingUser, userData} = useGetUser(address)
+
+    const onClick = () => {
+        open({view: address ? 'Account' : 'Connect'})
+    } 
+
+    useEffect(() => {
+        if(address) {
+            if(!isLoadingUser && (!userData || userData?.length === 0)) {
+                useCreateUserMutation.mutate({userAddress: address})
+            }
+        }
+    }, [address, isLoadingUser, userData])
+
+    return <Button onClick={onClick} colorScheme={'blue'}>
+        { address ? address : 'Connect Wallet'}
+    </Button>
+}
